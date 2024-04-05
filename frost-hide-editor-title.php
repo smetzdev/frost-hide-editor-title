de03a41735553170d450e1fe4cd1f483e2cec36f
Sascha Metz <hello@smetz.dev>
2024-04-05 11:11:26 +0200

    feat: :sparkles: add i18n support

--- frost-hide-editor-title.php
+++ frost-hide-editor-title.php
@@ -5,22 +5,39 @@
  * Author:          Sascha Metz
  * Author URI:      smetz.dev
  * Text Domain:     frost_hide_editor_title
- * Version:         0.1.0
+ * Domain Path:     /languages
+ * Version:         0.1.1
  */
 
+namespace smetzdev\FrostHideEditorTitle;
 
-function het_enqueue_assets() {
-    // Load the plugins js file.
-    wp_enqueue_script(
-        'het-editor-script', // Handle for the script.
-        plugins_url('/assets/editor.js', __FILE__), // Path to the script file.
-        array('wp-blocks', 'wp-dom', 'wp-edit-post') // Dependencies.
-    );
-
-    // Load the plugins css file.
-    wp_enqueue_style(
-        'het-editor-style', // Handle for the stylesheet.
-        plugins_url('/assets/editor.css', __FILE__), // Path to the stylesheet file.        
-    );
+/**
+ * Load the translations for the plugin.
+ */
+function het_load_translations() {
+	load_plugin_textdomain(
+		'frost_hide_editor_title',
+		false,
+		dirname( plugin_basename( __FILE__ ) ) . '/languages'
+	);
 }
-add_action('enqueue_block_editor_assets', 'het_enqueue_assets');
+add_action( 'init', 'smetzdev\FrostHideEditorTitle\het_load_translations' );
+
+/**
+ * Enqueue the assets for the editor screen.
+ */
+function het_enqueue_assets() {
+	// Load the plugins js file.
+	wp_enqueue_script(
+		'het-editor-script', // Handle for the script.
+		plugins_url( '/assets/editor.js', __FILE__ ), // Path to the script file.
+		array( 'wp-blocks', 'wp-dom', 'wp-edit-post' ) // Dependencies.
+	);
+
+	// Load the plugins css file.
+	wp_enqueue_style(
+		'het-editor-style', // Handle for the stylesheet.
+		plugins_url( '/assets/editor.css', __FILE__ ), // Path to the stylesheet file.
+	);
+}
+add_action( 'enqueue_block_editor_assets', 'smetzdev\FrostHideEditorTitle\het_enqueue_assets' );
